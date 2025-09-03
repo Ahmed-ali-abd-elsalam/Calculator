22a1066caecab6c123391d347c67621debb52ceb
Ahmed Abdesalam <thei3utcher2001@gmail.com>
2025-09-03 11:56:18 +0300

    Project Setup

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Calculator
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("Project setup");
+        }
+
+    }
+}
